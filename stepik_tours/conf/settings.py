d686e1c02305c26cd3267618af64ef8683fe2836
Nataliya Kashaeva <n.a.kashaeva@gmail.com>
2022-04-15 19:55:26 +0300

    Внесены изменения для деплоя с помощью Heroku

--- stepik_tours/conf/settings.py
+++ stepik_tours/conf/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-my&@7(nkj)!lrsh6*ot2etwa%295fc*6!ri!w(zc#ml2u8vtuy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,9 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "static"),
-]
+STATIC_ROOT = 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
